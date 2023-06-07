ebef07a89009e4e4506a8252d50139623f62a9a6
koome <koomemc@gmail.com>
2023-06-07 16:24:05 -0700

    initial commit

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: 1 if strings are identical 
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+			return (wildcmp(s1, s2 + 1));
+		return (*s2 == '\0');
+	}
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+
+	return (0);
+}
