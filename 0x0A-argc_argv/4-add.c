9338c22325866b36aecad3abd83699c6ec377bd3
koome <koomemc@gmail.com>
2023-06-11 15:02:21 -0700

    initial commit

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,54 @@
+#include <stdio.h> /* printf */
+#include <stdlib.h> /* atoi */
+#include <stdbool.h> /* bool data type */
+
+/**
+ * num - iterate through each argv to test if it's a number
+ * @argvv: a argv
+ * Return: true only if entire string is a number, false if not
+ */
+
+bool num(char *argvv)
+{
+	int i = 0;
+
+	for (i = 0; argvv[i]; i++)
+	{
+		if (!(argvv[i] >= '0' && argvv[i] <= '9'))
+			return (0);
+	}
+	return (1);
+}
+
+/**
+ * main - print sum if all arguments
+ * @argc: argument counter
+ * @argv: arguments
+ * Return: 0 on success, 1 if an argument wasn't a number
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 1;
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	while (i < argc)
+	{
+		if (num(argv[i]))
+			sum += atoi(argv[i]);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		i++;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
