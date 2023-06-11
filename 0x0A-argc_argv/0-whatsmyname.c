0a1aa2f5122836a8866a68e8d01020cff0235614
koome <koomemc@gmail.com>
2023-06-11 13:51:53 -0700

    Initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints name of program
+ * @argc: number of arguments
+ * @argv: pointer to array of arguments
+ * Return: returns 0 if successful
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+	printf("%s \n", argv[0]);
+	}
+	return (0);
+}
