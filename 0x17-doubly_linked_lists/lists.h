337808c7f9b06808e81d36d70199f9e3a74cae11
koome <koomemc@gmail.com>
2023-08-17 14:22:05 -0700

    add libraries to header file

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,9 @@
 #ifndef LIST_H
 #define LIST_H
 
+#include <stdio.h> /* printf */
+#include <stdlib.h> /* malloc, free */
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -25,5 +28,5 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
-
+dlistint_t *insert_node(dlistint_t *tmp, int n);
 #endif
