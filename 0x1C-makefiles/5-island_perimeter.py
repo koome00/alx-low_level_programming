0597338e008bdd94ffa76719a7418e1ae1d7b83c
koome <koomemc@gmail.com>
2023-10-25 19:06:33 +0300

    find perimeter task

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+"""
+ returns the perimeter of the island described in grid:
+
+grid is a list of list of integers:
+0 represents a water zone
+1 represents a land zone
+One cell is a square with side length 1
+Grid cells are connected horizontally/vertically (not diagonally).
+Grid is rectangular, width and height don’t exceed 100
+
+"""
+
+def island_perimeter(grid):
+    total_peri = 0
+    for x, row in enumerate(grid):
+        for y, element in enumerate(row):
+            if grid[x][y] == 1:
+                peri = 4
+                if grid[x+1][y] == 1:
+                    peri = peri - 1
+                if grid[x-1][y] == 1:
+                    peri = peri - 1
+                if grid[x][y+1] == 1:
+                    peri = peri - 1
+                if grid[x][y-1] == 1:
+                    peri = peri - 1
+                total_peri = peri + total_peri
+    print(total_peri)
