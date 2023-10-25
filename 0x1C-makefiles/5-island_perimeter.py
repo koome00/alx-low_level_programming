d5eefacd66a5703d75fd2c773efcd5bcd699634f
koome <koomemc@gmail.com>
2023-10-25 20:45:53 +0300

    update to account for last row and column

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -17,13 +17,20 @@ def island_perimeter(grid):
         for y, element in enumerate(row):
             if element == 1:
                 peri = 4
-                if grid[x+1][y] == 1:
+                # check bottom upto second last row
+                if grid[x+1][y] == 1 and x < (len(grid)-1):
                     peri = peri - 1
-                if grid[x-1][y] == 1:
+                # check right upto second last column
+                if grid[x][y+1] == 1 and y < (len(grid)-1):
                     peri = peri - 1
-                if grid[x][y+1] == 1:
+                
+                # check top upto second last row
+                if grid[x-1][y] == 1 and x > 0:
                     peri = peri - 1
-                if grid[x][y-1] == 1:
+                # check left upto second last column
+                if grid[x][y-1] == 1 and y > 0:
                     peri = peri - 1
+                              
                 total_peri = peri + total_peri
     print(total_peri)
+
